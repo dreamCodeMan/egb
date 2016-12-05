06a4fdfb3b9c64341cad99a92003a13ac489392e
angelinazf <805860735@qq.com>
2016-12-05 12:31:27 +0800

    重新对image_process中的函数进行重构,修复了file未关闭的问题,以及增加test函数

--- image_process.go
+++ image_process.go
@@ -7,8 +7,6 @@ import (
 	"image/gif"
 	"image/jpeg"
 	"image/png"
-	"io"
-	"log"
 	"math"
 	"os"
 	"path"
@@ -20,69 +18,83 @@ import (
 
 //ImageProcessor 图片剪裁参数
 type ImageProcessor struct {
-	Leftpoint int
-	Toppoint  int
+	//距离左边距离
+	LeftPoint int
+	//距离上边距离
+	TopPoint  int
+	//裁剪后的宽
 	Width     int
+	//剪裁后的高
 	Height    int
 }
 
-//Resize 缩放图片
-//InterpolationFunction的多种分类:
-//NearestNeighbor,Bilinear,Bicubic,MitchellNetravali,Lanczos2,Lanczos3
-func Resize(width, height uint, img image.Image, interp resize.InterpolationFunction) image.Image {
-	outImagePointer := resize.Resize(width, height, img, interp)
-	return outImagePointer
+//ResizeImage 图片缩放
+//@params	inputImagePath width height outputFileName
+//@return	error
+func ResizeImage(inputImagePath string, width, height uint, outputFileName string) error {
+	inputImage, err := LoadImage(inputImagePath)
+	if err != nil {
+		return err
+	}
+	outputImage := resize.Resize(width, height, inputImage, resize.Lanczos3)
+	err = SaveImage(outputFileName, outputImage)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 //GetImageInfo 获取图片的宽高信息
-//传入图片的存储路径 以及 图片的name    返回图片的 宽 高
-func GetImageInfo(imagePath string, imageName string) (width int, height int) {
-	imageRealPath := imagePath + imageName
-	imageFile, err := os.Open(imageRealPath)
-	defer imageFile.Close()
-	if err != nil {
-		panic(err)
+//@params	inputFilePath
+//@return	(int,int,error)
+func GetImageInfo(inputFilePath string) (int, int, error) {
+	if !FileExists(inputFilePath) {
+		return 0, 0, errors.New("图片文件不存在,请检查地址")
 	}
+	imageFile, err := os.Open(inputFilePath)
+	if err != nil {
+		return 0, 0, err
+	}
+	defer imageFile.Close()
 	img, _, err := image.Decode(imageFile)
 	if err != nil {
-		panic(err)
+		return 0, 0, err
 	}
 	imgWidth := img.Bounds().Max.X
 	imgHeight := img.Bounds().Max.Y
-	return imgWidth, imgHeight
+	return imgWidth, imgHeight, nil
 }
 
 //ProcessImage 剪裁图片
 //传入图片的存储路径 name  以及剪裁参数     返回图片的name
-func ProcessImage(imagePath, imageName string, processConfig ImageProcessor) (isSuc bool, getOutImageName string) {
-	imageExt := FileGetExt(imageName)
-	if imagePath == "" || imageName == "" {
-		return false, "图片地址错误"
+func ProcessImage(inputFilePath string, processConfig ImageProcessor) error {
+	if !FileExists(inputFilePath) {
+		return errors.New("图片文件不存在,请检查地址")
 	}
 	if processConfig.Width == 0 && processConfig.Height == 0 {
-		return false, "剪裁参数错误"
+		return errors.New("剪裁参数错误")
 	}
+	imageExt := FileGetExt(inputFilePath)
 	if imageExt != "png" && imageExt != "jpg" && imageExt != "jpeg" && imageExt != "gif" {
-		return false, "不能识别该后缀图片"
+		return errors.New("图片后缀必须是png,jpg,jpeg,gif中的一种")
 	}
 	var cutImageWidth = processConfig.Width
 	var cutImageHeight = processConfig.Height
-	var cutLeftPoint = processConfig.Leftpoint
-	var cutTopPoint = processConfig.Toppoint
-	imageRealPath := imagePath + imageName
-	imageFile, err := os.Open(imageRealPath)
+	var cutLeftPoint = processConfig.LeftPoint
+	var cutTopPoint = processConfig.TopPoint
+	imageFile, err := os.Open(inputFilePath)
 	if err != nil {
-		return false, err.Error()
+		return err
 	}
+	defer imageFile.Close()
 	img, _, err := image.Decode(imageFile)
 	if err != nil {
-		return false, err.Error()
+		return err
 	}
 	imgWidth := img.Bounds().Max.X
 	imgHeight := img.Bounds().Max.Y
-
 	if cutImageWidth > imgWidth && cutImageHeight > imgHeight {
-		return true, imageName
+		return nil
 	}
 	//	resizeWidth, resizeHeight := GetCutImageWidthAndHeight(imgWidth, imgHeight, cutImageWidth, cutImageHeight)
 	//等比缩放图片
@@ -93,7 +105,7 @@ func ProcessImage(imagePath, imageName string, processConfig ImageProcessor) (is
 		outImagePointer, _ = cutter.Crop(img, cutter.Config{
 			Width:  cutImageWidth,
 			Height: cutImageHeight,
-			Anchor: image.Point{cutLeftPoint, cutTopPoint},
+			Anchor: image.Point{X:cutLeftPoint, Y:cutTopPoint},
 			Mode:   cutter.TopLeft, // optional, default value
 		})
 	}
@@ -108,117 +120,81 @@ func ProcessImage(imagePath, imageName string, processConfig ImageProcessor) (is
 		draw.Draw(jpg, jpg.Bounds(), outImagePointer, outImagePointer.Bounds().Min.Sub(image.Pt(0, -cutTopPoint)), draw.Over)
 	}
 	//输出图片
-	outputImage, err := os.Create(imageRealPath)
+	outputImage, err := os.Create(inputFilePath)
 	if err != nil {
-		return false, err.Error()
+		return err
 	}
+	defer outputImage.Close()
 	if cutLeftPoint >= 0 && cutTopPoint >= 0 {
 		switch imageExt {
 		case "jpg", "jpeg":
 			err = jpeg.Encode(outputImage, outImagePointer, nil)
 			if err != nil {
-				return false, err.Error()
+				return err
 			}
 		case "png":
-			png.Encode(outputImage, outImagePointer)
+			err = png.Encode(outputImage, outImagePointer)
+			if err != nil {
+				return err
+			}
 		case "gif":
-			gif.Encode(outputImage, outImagePointer, nil)
+			err = gif.Encode(outputImage, outImagePointer, nil)
+			if err != nil {
+				return err
+			}
 		}
 	} else {
 		switch imageExt {
 		case "jpg", "jpeg":
 			err = jpeg.Encode(outputImage, jpg, nil)
 			if err != nil {
-				return false, err.Error()
+				return err
 			}
 		case "png":
-			png.Encode(outputImage, jpg)
+			err = png.Encode(outputImage, jpg)
+			if err != nil {
+				return err
+			}
 		case "gif":
-			gif.Encode(outputImage, jpg, nil)
+			err = gif.Encode(outputImage, jpg, nil)
+			if err != nil {
+				return err
+			}
 		}
 	}
-
-	return true, imageName
+	return nil
 }
 
-//GetCutImageWidthAndHeight 通过计算图片缩放比，返回缩放后的宽高
-func GetCutImageWidthAndHeight(inputWidth int, inputHeight int, cutWidth int, cutHeight int) (width int, height int) {
-	if cutWidth == 0 && cutHeight == 0 {
-		return inputWidth, inputHeight
-	}
-	if inputWidth < cutWidth || inputHeight < cutHeight {
-		return inputWidth, inputHeight
-	}
-	if cutWidth == 0 {
-		return int(float32(inputWidth) / (float32(inputHeight) / float32(cutHeight))), cutHeight
-	}
-	if cutHeight == 0 {
-		return cutWidth, int(float32(inputHeight) / (float32(inputWidth) / float32(cutWidth)))
-	}
-	if float32(inputWidth)/float32(cutWidth) == float32(inputHeight)/float32(cutHeight) {
-		return cutWidth, cutHeight
-	}
-	if float32(inputWidth)/float32(cutWidth) > float32(inputHeight)/float32(cutHeight) {
-		width = int(float32(inputWidth) / (float32(inputHeight) / float32(cutHeight)))
-		height = cutHeight
-	} else {
-		width = cutWidth
-		height = int(float32(inputHeight) / (float32(inputWidth) / float32(cutWidth)))
-	}
-	return width, height
-}
-
-//CopyImage 拷贝一份图片
-func CopyImage(imagePath, imageName, copyName string) (img string) {
-	inputImageRealPath := imagePath + imageName
-	r, err := os.Open(inputImageRealPath)
-	if err != nil {
-		panic(err)
-	}
-	outImageRealPath := imagePath + copyName
-	w, err := os.Create(outImageRealPath)
-	defer w.Close()
-	if err != nil {
-		panic(err)
-	}
-	_, err = io.Copy(w, r)
-	if err != nil {
-		panic(err)
-	}
-	return copyName
-}
-
-//RotateImage 旋转图片  参数为角度
-//旋转后图片大小
-//newW = w * cos(angle) + h * sin(angle)
-//newH = w * sin(angle) + h * cos(angle)
-func RotateImage(imagePath, imageName string, angle float64) (err error) {
-	if imagePath == "" || imageName == "" {
-		e := errors.New("图片地址错误")
-		return e
+//RotateImage 旋转图片  参数为角度(顺时针)
+//@params	inputImagePath outputImagePath angle
+//@return	error
+func RotateImage(inputImagePath, outputImagePath string, angle float64) error {
+	if !FileExists(inputImagePath) {
+		return errors.New("图片地址错误,请检查")
 	}
 	angle = (float64(angle) * math.Pi) / 180
-	imageExt := FileGetExt(imageName)
+	imageExt := FileGetExt(inputImagePath)
 	if imageExt != "png" && imageExt != "jpg" && imageExt != "jpeg" && imageExt != "gif" {
-		e := errors.New("不能识别该后缀图片")
-		return e
+		return errors.New("图片后缀必须是png,jpg,jpeg,gif中的一种")
 	}
-	imgRealPath := imagePath + imageName
-	src, e := LoadImage(imgRealPath)
-	if e != nil {
-		return e
+	srcImage, err := LoadImage(inputImagePath)
+	if err != nil {
+		return err
 	}
-	w, h := GetImageInfo(imagePath, imageName)
-	newwidth := float64(w)*math.Cos(angle) + float64(h)*math.Sin(angle)
-	newheight := float64(w)*math.Sin(angle) + float64(h)*math.Cos(angle)
-	dst := image.NewRGBA(image.Rect(0, 0, int(newwidth), int(newheight)))
-	e = graphics.Rotate(dst, src, &graphics.RotateOptions{angle})
-	if e != nil {
-		return e
+	w, h, err := GetImageInfo(inputImagePath)
+	if err != nil {
+		return err
 	}
-	e = SaveImage(imgRealPath, dst)
-	if e != nil {
-		return e
+	newWidth := float64(w) * math.Cos(angle) + float64(h) * math.Sin(angle)
+	newHeight := float64(w) * math.Sin(angle) + float64(h) * math.Cos(angle)
+	dst := image.NewRGBA(image.Rect(0, 0, int(newWidth), int(newHeight)))
+	err = graphics.Rotate(dst, srcImage, &graphics.RotateOptions{Angle:angle})
+	if err != nil {
+		return err
+	}
+	err = SaveImage(outputImagePath, dst)
+	if err != nil {
+		return err
 	}
 	return nil
 }
@@ -235,43 +211,64 @@ func LoadImage(path string) (img image.Image, err error) {
 }
 
 //SaveImage 将image.image保存进某个path  以png格式
-func SaveImage(path string, img image.Image) (err error) {
-	imgfile, err := os.Create(path)
-	defer imgfile.Close()
-	// 以PNG格式保存文件
-	err = png.Encode(imgfile, img)
+//@params	path img
+//@return	error
+func SaveImage(path string, img image.Image) error {
+	imgFile, err := os.Create(path)
 	if err != nil {
-		return
+		return err
 	}
-	return
+	defer imgFile.Close()
+	// 以PNG格式保存文件
+	err = png.Encode(imgFile, img)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-//ImageThumbnail 图片缩略
-//@params
-//@return
-func ImageThumbnail(file string, width uint, height uint, to string) {
-	// 打开图片并解码
-	fileOrigin, _ := os.Open(file)
-	switch path.Ext(file) {
-	case ".jpeg", ".jpg":
-		origin, _ := jpeg.Decode(fileOrigin)
-		defer fileOrigin.Close()
-		canvas := resize.Thumbnail(width, height, origin, resize.Lanczos3)
-		fileOut, err := os.Create(to)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer fileOut.Close()
-		jpeg.Encode(fileOut, canvas, &jpeg.Options{Quality: 80})
-	case ".png":
-		origin, _ := png.Decode(fileOrigin)
-		defer fileOrigin.Close()
-		canvas := resize.Thumbnail(width, height, origin, resize.Lanczos3)
-		fileOut, err := os.Create(to)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer fileOut.Close()
-		png.Encode(fileOut, canvas)
+//ImageThumbnail 生成缩略图  最大宽高,不会改变图片的比例
+//@params	inputFile maxWidth maxHeight outputFile
+//@return	error
+func ThumbImage(inputFileName string, maxWidth, maxHeight uint, outputFileName string) error {
+	inputFile, err := os.Open(inputFileName)
+	if err != nil {
+		return err
 	}
+	defer inputFile.Close()
+	switch path.Ext(inputFileName) {
+	case ".jpg", ".jpeg":
+		inputImage, err := jpeg.Decode(inputFile)
+		if err != nil {
+			return err
+		}
+		newImage := resize.Thumbnail(maxWidth, maxHeight, inputImage, resize.Lanczos3)
+		outputFile, err := os.Create(outputFileName)
+		if err != nil {
+			return err
+		}
+		defer outputFile.Close()
+		err = jpeg.Encode(outputFile, newImage, &jpeg.Options{Quality:80})
+		if err != nil {
+			return err
+		}
+	case ".png":
+		inputImage, err := png.Decode(inputFile)
+		if err != nil {
+			return err
+		}
+		newImage := resize.Thumbnail(maxWidth, maxHeight, inputImage, resize.Lanczos3)
+		outputFile, err := os.Create(outputFileName)
+		if err != nil {
+			return err
+		}
+		defer outputFile.Close()
+		err = png.Encode(outputFile, newImage)
+		if err != nil {
+			return err
+		}
+	default:
+		return errors.New("图片文件后缀错误,请传入png,jpg,jpeg图片")
+	}
+	return nil
 }
---
+++ image_process_test.go
@@ -0,0 +1,39 @@
+/**
+ * Created by angelina-zf on 16/12/5.
+ */
+package egb
+
+import "testing"
+
+const (
+	inputImagePath = "/Users/angelina-zf/Playground/golib/src/github.com/agelinazf/egb/testdata/image/1.jpg"
+	outputResizeImagePath = "/Users/angelina-zf/Playground/golib/src/github.com/agelinazf/egb/testdata/image/1_resize.jpg"
+	outputThumbImagePath = "/Users/angelina-zf/Playground/golib/src/github.com/agelinazf/egb/testdata/image/1_thumb.jpg"
+)
+
+func TestResizeImage(t *testing.T) {
+	err := ResizeImage(inputImagePath, 200, 200, outputResizeImagePath)
+	if err != nil {
+		t.Fatal("error : " + err.Error())
+	}
+}
+
+func TestThumbImage(t *testing.T) {
+	err := ThumbImage(inputImagePath, 200, 200, outputThumbImagePath)
+	if err != nil {
+		t.Fatal("error : " + err.Error())
+	}
+}
+
+func TestGetImageInfo(t *testing.T) {
+	w, h, err := GetImageInfo(outputThumbImagePath)
+	if err != nil {
+		t.Fatal("error : " + err.Error())
+	}
+	if w != 200 {
+		t.Errorf("width should be 200 but get %d", w)
+	}
+	if h != 112 {
+		t.Errorf("height should be 112 but get %d", h)
+	}
+}
